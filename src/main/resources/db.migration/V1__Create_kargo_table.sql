4da930f30837577fd1de8c035636dbf62eeb159d
yarentag <yarentag23@gmail.com>
2024-10-20 20:01:47 +0300

    PostgreSQL bağlantısı ve Flyway migration ayarları eklendi

---
+++ src/main/resources/db.migration/V1__Create_kargo_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE kargo (
+                       id SERIAL PRIMARY KEY,
+                       musteri_adi VARCHAR(255),
+                       adres TEXT,
+                       teslim_tarihi TIMESTAMP
+);
